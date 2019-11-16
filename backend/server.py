5662b3a7713e0c2655bf44aa438f1df15b6e56ab
gbnegrini <negrini.guilherme@gmail.com>
2019-11-16 15:22:48 -0300

    Front and backend

--- backend/server.py
+++ backend/server.py
@@ -2,8 +2,10 @@ from flask import Flask, request, jsonify
 import lyricsgenius
 from better_profanity import profanity
 from genius_token import genius_token
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/api/v1/lyrics', methods=['GET', 'POST'])
 def get_lyrics():
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import 'bootswatch/dist/sketchy/bootstrap.min.css';
 import './App.css';
+import Nav from './components/Nav.js'
+import api from './services/api'
 
 function App() {
+    const [artist, setArtist] = useState('');
+    const [song, setSong] = useState('');
+    const [lyrics, setLyrics] = useState('');
+    const [safe, setSafe] = useState('');
+
+    async function handleSubmit(e){
+        e.preventDefault();
+        const response = await api.get('/lyrics', {
+            params:{
+                artist: artist,
+                song: song
+            }
+        });
+
+        let lyrics = response.data.lyrics.split('\n').map((item, i) => <p key={i}>{item}</p>);
+        setLyrics(lyrics);
+
+        if(response.data.safe){
+            setSafe(
+            <div className="alert alert-dismissible alert-success">
+                These lyrics are <strong>probably</strong> free of offensive language
+            </div>);
+        }else{
+            setSafe(
+            <div className="alert alert-dismissible alert-danger">
+                These lyrics <strong>probably</strong> contain offensive language
+            </div>);
+        }
+    }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <React.Fragment>
+    <div>
+        <Nav brand='Safe Lyrics' homeLink='#' aboutLink='#'/>
     </div>
+    <div className='wrap'>
+        <div className='row'>
+          <div className='col'>
+            <div>
+                <form onSubmit={handleSubmit}>
+                    <fieldset>
+                      <legend>Search</legend>
+                      <input
+                          className="form-control form-control-lg"
+                          type="text"
+                          placeholder="Artist"
+                          id="artist"
+                          value={artist}
+                          onChange={event => setArtist(event.target.value)}>
+                      </input>
+                      <div className="form-group">
+                      </div>
+                      <div className="form-group">
+                        <input
+                            className="form-control form-control-lg"
+                            type="text"
+                            placeholder="Song"
+                            id="song"
+                            value={song}
+                            onChange={event => setSong(event.target.value)}>
+                        </input>
+                      </div>
+                      <button type="submit" className="btn btn-primary">Search</button>
+                    </fieldset>
+                  </form>
+            </div>
+          </div>
+          <div className='col'>
+            <div className='lyr'>
+              <h2>Lyrics</h2>
+              <div>{safe}</div>
+              <p>{lyrics}</p>
+            </div>
+          </div>
+        </div>
+  </div>
+  </React.Fragment>
   );
 }
 
